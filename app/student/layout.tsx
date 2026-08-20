5af4dd42f5a7de9d7a3f92b079896fdefe77479d
Prince <iamprinceasante@gmail.com>
2026-08-20 17:23:08 +0000

    Got rid of navbar

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -12,16 +12,16 @@ export default function Student_Layout({ children }: LayoutProps<"/">) {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <div className="z-20 py-2 px-4 h-20 sticky top-0">
+          {/* <div className="z-20 py-2 px-4 h-20 sticky top-0">
             <div className="absolute bg-white h-4 -ml-4 w-full z-2 top-0"></div>
-            <header className="relative z-3 flex h-16 shrink-0 items-center gap-2 bg-primary-foreground/80 border rounded-lg shadow backdrop-blur px-4">
+            <header className="relative z-3 flex h-16 shrink-0 items-center gap-2 bg-primary-foreground/80 backdrop-blur px-4 border rounded-lg shadow">
               <SidebarTrigger className="-ml-1 text-blue-600/80 hover:text-blue-600 hover:bg-paper-dim" />
               <Separator
                 orientation="vertical"
                 className="mr-2 data-[orientation=vertical]:h-8 mt-3.5"
               />
             </header>
-          </div>
+          </div> */}
 
           <main className="p-4 z-10 w-full">{children}</main>
         </SidebarInset>
